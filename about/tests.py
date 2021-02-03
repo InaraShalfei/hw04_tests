db31e618e4d39427d52162f2b2ebddee353cd003
Инара <inarashugaikina@gmail.com>
2021-02-03 17:24:50 +0600

    Create tests for About app

--- about/tests.py
+++ about/tests.py
@@ -1,3 +1,28 @@
-from django.test import TestCase
+from django.test import Client, TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class StaticViewsTests(TestCase):
+    def setUp(self):
+        self.guest_client = Client()
+
+    def test_about_author_page_accessible_for_user(self):
+        response = self.guest_client.get(reverse('about:author'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_about_author_page_uses_correct_template(self):
+        response = self.guest_client.get(reverse('about:author'))
+        self.assertTemplateUsed(response, 'about/author.html')
+
+    def test_about_tech_page_accessible_for_user(self):
+        response = self.guest_client.get(reverse('about:tech'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_about_tech_page_uses_correct_template(self):
+        response = self.guest_client.get(reverse('about:tech'))
+        self.assertTemplateUsed(response, 'about/tech.html')
+
+
+
+
+
