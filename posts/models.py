e7b66df025db6b5e30cddf8c1d8bd34737965ce7
Инара <inarashugaikina@gmail.com>
2021-02-04 23:19:46 +0600

    Set lines size acc to PEP8 in posts/models.py

--- posts/models.py
+++ posts/models.py
@@ -6,9 +6,12 @@ User = get_user_model()
 
 
 class Group(models.Model):
-    title = models.CharField("Название группы", max_length=200, help_text="Дать название группе")
-    slug = models.SlugField("Адрес группы", unique=True, help_text="Указать адрес для страницы")
-    description = models.TextField("Описание группы", help_text="Дать описание группе")
+    title = models.CharField("Название группы",
+                             max_length=200, help_text="Дать название группе")
+    slug = models.SlugField("Адрес группы",
+                            unique=True, help_text="Указать адрес для страницы")
+    description = models.TextField("Описание группы",
+                                   help_text="Дать описание группе")
 
     class Meta:
         verbose_name = "Группа"
@@ -19,15 +22,19 @@ class Group(models.Model):
 
 
 class Post(models.Model):
-    text = models.TextField("Текст поста", help_text="Написать текст поста")
-    pub_date = models.DateTimeField("Дата публикации", auto_now_add=True)
+    text = models.TextField("Текст поста",
+                            help_text="Написать текст поста")
+    pub_date = models.DateTimeField("Дата публикации",
+                                    auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE,
                                related_name="posts",
-                               verbose_name="Автор поста", help_text="Указать автора поста")
+                               verbose_name="Автор поста",
+                               help_text="Указать автора поста")
     group = models.ForeignKey(Group, on_delete=models.SET_NULL,
                               related_name="posts",
                               blank=True,
-                              verbose_name="Группа поста", null=True, help_text="Выбрать группу поста")
+                              verbose_name="Группа поста",
+                              null=True, help_text="Выбрать группу поста")
 
     class Meta:
         ordering = ["-pub_date"]
