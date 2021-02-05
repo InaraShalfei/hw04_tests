c8e35a01f687ead82533c9c85396f7688fe5af30
Инара <inarashugaikina@gmail.com>
2021-02-05 21:35:13 +0600

    Add PostForm(request.POST or None) into views.py

--- posts/views.py
+++ posts/views.py
@@ -48,13 +48,10 @@ def post_edit(request, username, post_id):
     if request.user.username != username:
         return redirect("post", username=username, post_id=post_id)
     post = get_object_or_404(Post, id=post_id, author__username=username)
-    if request.method == "POST":
-        form = PostForm(request.POST, instance=post)
-        if form.is_valid():
-            form.save()
-            return redirect("post", username=username, post_id=post_id)
-        return render(request, "new.html", {"username": username,
-                                            "post": post, "form": form})
+    form = PostForm(request.POST or None, instance=post)
+    if form.is_valid():
+        form.save()
+        return redirect("post", username=username, post_id=post_id)
     form = PostForm(instance=post)
     return render(request, "new.html", {"username": username,
                                         "post": post, "form": form})
@@ -62,13 +59,11 @@ def post_edit(request, username, post_id):
 
 @login_required
 def new_post(request):
-    if request.method == "POST":
-        form = PostForm(request.POST)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            form.save()
-            return redirect("index")
-        return render(request, "new.html", {"form": form})
+    form = PostForm(request.POST or None)
+    if form.is_valid():
+        post = form.save(commit=False)
+        post.author = request.user
+        form.save()
+        return redirect("index")
     form = PostForm()
     return render(request, "new.html", {"form": form})
