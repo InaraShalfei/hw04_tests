caa18c874e2e33e27f34134f4c82bf2d021cee25
Инара <inarashugaikina@gmail.com>
2021-02-07 21:04:28 +0600

    Delete unused variable from post_view

--- posts/views.py
+++ posts/views.py
@@ -39,9 +39,8 @@ def profile(request, username):
 def post_view(request, username, post_id):
     user = get_object_or_404(User, username=username)
     post = get_object_or_404(Post, id=post_id, author=user)
-    posts_count = user.posts.count()
     return render(request, "post.html", {"username": user,
-                                         "post": post, "posts_count": posts_count})
+                                         "post": post})
 
 
 def post_edit(request, username, post_id):
