bb88e7f42bca3edb3c84666b205df910d355c141
Инара <inarashugaikina@gmail.com>
2021-02-10 16:43:19 +0600

    Create test for checking image in form

--- posts/tests/test_forms.py
+++ posts/tests/test_forms.py
@@ -1,8 +1,14 @@
+import shutil
+import tempfile
+
+from django.conf import settings
+from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import Client, TestCase
 from django.urls import reverse
 from django.contrib.auth import get_user_model
 
 from posts.models import Post, Group
+
 
 User = get_user_model()
 
@@ -11,12 +17,20 @@ class NewPostFormTest(TestCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
+        settings.MEDIA_ROOT = tempfile.mkdtemp(dir=settings.BASE_DIR)
+
+        cls.user = User.objects.create_user('Dike', 'admin@test.com', 'pass')
 
         cls.group = Group.objects.create(
             title='Vsem privet',
             slug='test-slug',
             description='Gruppa chtoby govorit privet',
         )
+
+    @classmethod
+    def tearDownClass(cls):
+        shutil.rmtree(settings.MEDIA_ROOT, ignore_errors=True)
+        super().tearDownClass()
 
     def setUp(self):
         self.user = get_user_model().objects.create_user(username='Mike')
@@ -25,8 +39,23 @@ class NewPostFormTest(TestCase):
 
     def test_new_post_page_create(self):
         posts_count = Post.objects.count()
+        small_gif = (
+            b'\x47\x49\x46\x38\x39\x61\x02\x00'
+            b'\x01\x00\x80\x00\x00\x00\x00\x00'
+            b'\xFF\xFF\xFF\x21\xF9\x04\x00\x00'
+            b'\x00\x00\x00\x2C\x00\x00\x00\x00'
+            b'\x02\x00\x01\x00\x00\x02\x02\x0C'
+            b'\x0A\x00\x3B'
+        )
+        uploaded = SimpleUploadedFile(
+            name='small.gif',
+            content=small_gif,
+            content_type='image/gif'
+        )
         form_data = {'group': NewPostFormTest.group.id,
-                     'text': 'Текст, который мы заслужили'}
+                     'text': 'Текст, который мы заслужили',
+                     'image': uploaded,
+                     }
         response = self.authorized_client.post(
             reverse('new_post'),
             data=form_data,
